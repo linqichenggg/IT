077f553515365a6fedc77ea719bdbe9b05be4e1e
LINQC <linqicheng65@gmail.com>
2023-10-29 11:34:01 +0800

    由于打开后浏览器会自动关闭，回退selenium版本

--- crawler.py
+++ crawler.py
@@ -1,7 +1,8 @@
 # import selenium
 from selenium import webdriver
 
+PATH = ""
 # browser = webdriver.Edge()
-driver = webdriver.Edge()
+driver = webdriver.Edge("C:\Program Files (x86)\Microsoft\Edge\Application\msedgedriver.exe")
 
 driver.get("https://google.com")
